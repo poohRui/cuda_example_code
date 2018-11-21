3bd3c47fcf3363e1156f4bad0e3bd2c4adb56e0e
poohRui <yuruil@qq.com>
2018-11-21 11:33:54 +0800

    modify some funtion of vector add example

---
+++ vector_add/ReadMe.md
@@ -0,0 +1,6 @@
+# Vector Add Example
+> 通过串行和并行两种方法，对向量相加进行实现，并分别输出其结果和运行时间。
+
+vecAddSerial：串行向量相加函数
+vecAdd：并行程序的stub函数
+vecAddKernel：向量相加的kernel。
--- vector_add/main.cpp
+++ vector_add/main.cpp
@@ -11,24 +11,20 @@
 #include "vectorAdd.h"
 using namespace std;
 
+// Predefine the size of the two vectors
 #define N 400
 
 /**
- * This is a function to randomly initial the data in vector A and vector B
+ * This is a function to randomly initial the data in vector
  *
- * @param A  One of the vector to be add on device
- * @param B  One of the vector to be add on device
+ * @param v  One of the vector to be add on device
  * @param n  The lenght of the vector
  */
-void initialVector(float* h_A,
-                   float* h_B,
+void initialVector(float* v,
                    int    n){
     
-    srand(time(NULL));
-    
     for(int i = 0;i<n;i++){
-        h_A[i] = rand() % 100;
-        h_B[i] = rand() % 100;
+        v[i] = rand() % 100;
     }
 }
 
@@ -39,31 +35,42 @@ void vecAddSerial(float* A, float* B, float* C, int n){
 }
 
 int main(){
-    // Memory allocation for h_A, h_B, and h_C
+    // Memory allocation for h_A, h_B which contain the vector A and B
     float* h_A = new float[N];
     float* h_B = new float[N];
-    float* h_C = new float[N];
+
+    // Memory allocation for serial_C and parallel_C which contain the result of serial and parallel
+    float* serial_C = new float[N];
+    float* parallel_C = new float[N];
     
-    // I/O to read h_A and h_B, N elements each
-    initialVector(h_A, h_B, N);
+    // Random initial h_A and h_B, N elements each
+    srand(time(NULL));
+    initialVector(h_A, N);
+    initialVector(h_B, N);
     
     // Invoke the stub funtion(parallel)
-    vecAdd(h_A, h_B, h_C, N);
+    vecAdd(h_A, h_B, parallel_C, N);
+
+    // Show the parallel result
+    for(int i = 0;i < N;i++){
+        cout<<h_A[i]<<" + "<<h_B[i]<<" = "<<parallel_C[i]<<endl;
+    }
     
     // Using the traditional serial code and finally print the time
     clock_t serial_start,serial_end;
     serial_start = clock();
     
     // Invoke the vecAdd serial function
-    vecAddSerial(h_A, h_B, h_C, N);
+    vecAddSerial(h_A, h_B, serial_C, N);
     
     serial_end = clock();
     double dur = (double)(serial_end - serial_start);
     cout<<"Serial invoke vectorAdd function need "<<dur/CLOCKS_PER_SEC<<"s."<<endl;
     
-    // Show the result
-    /* for(int i = 0;i < N;i++){
-     cout<<h_A[i]<<" + "<<h_B[i]<<" = "<<h_C[i]<<endl;
-     }*/
+    // Show the parallel result
+    for(int i = 0;i < N;i++){
+        cout<<h_A[i]<<" + "<<h_B[i]<<" = "<<serial_C[i]<<endl;
+    }
+    
 }
 
